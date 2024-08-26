e9168744e95275c9a5ba227101ea7b0831d8013b
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-08-27 03:44:30 +0800

    Add files via upload

--- project/androidtools/src/main/java/org/haxe/extension/Details.java
+++ project/androidtools/src/main/java/org/haxe/extension/Details.java
@@ -3,7 +3,9 @@ package org.haxe.extension;
 import org.haxe.extension.Extension;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Build;
 
 public class Details extends Extension {
 
@@ -20,6 +22,20 @@ public class Details extends Extension {
             return mainContext.getPackageManager().getPackageInfo(mainContext.getPackageName(), 0).versionCode;
         } catch (PackageManager.NameNotFoundException e) {
             throw new RuntimeException("Could not get version code", e);
+        }
+    }
+
+    public static String getPackageName() {
+        return mainContext.getPackageName();
+    }
+
+    public static String getAppName() {
+        try {
+            PackageInfo packageInfo = mainContext.getPackageManager()
+                    .getPackageInfo(mainContext.getPackageName(), 0);
+            return packageInfo.applicationInfo.loadLabel(mainContext.getPackageManager()).toString();
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException("Could not get app name", e);
         }
     }
 }
