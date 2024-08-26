a5c28dffe20879e516aa39dc32662afe97ae9233
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-08-27 02:02:45 +0800

    Add files via upload

--- android/os/AppDetails.hx
+++ android/os/AppDetails.hx
@@ -7,8 +7,28 @@ package android.os;
 import lime.system.JNI;
 
 class AppDetails {
-     public static function getAppVersionName():Int 
-        return JNI.createStaticMethod("org.haxe.extension.Details", "getVersionName", "(Landroid/content/Context;)Ljava/lang/String;");
-     public static function getAppName():String 
-        return JNI.createStaticMethod("org.haxe.extension.Details", "getVersionCode", "(Landroid/content/Context;)I");       
+     package org.haxe.extension;
+
+import lime.system.JNI;
+
+class VersionDetails {
+    public static function getVersionName():String {
+        var className = "org/haxe/extension/Details";
+        var methodName = "getVersionName";
+        
+        var method = JNI.createStaticMethod(className, methodName, "()Ljava/lang/String;");
+        
+        return method;
+    }
+    
+    public static function getVersionCode():Int {
+        var className = "org/haxe/extension/Details";
+        var methodName = "getVersionCode";
+        
+        var method = JNI.createStaticMethod(className, methodName, "()I");
+        
+        return method;
+    }
+}
+
 }
