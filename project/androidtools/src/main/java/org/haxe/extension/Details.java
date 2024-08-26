0a35c6d06d3c232e93b5ab7f715ef061d5f0826f
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2024-08-27 01:59:21 +0800

    Add files via upload

--- project/androidtools/src/main/java/org/haxe/extension/Details.java
+++ project/androidtools/src/main/java/org/haxe/extension/Details.java
@@ -1,21 +1,23 @@
 package org.haxe.extension;
 
+import org.haxe.extension.Extension;
+
 import android.content.Context;
 import android.content.pm.PackageManager;
 
-public class Details extends android.app.Activity {
+public class Details extends Extension {
 
-    public static String getVersionName(Context context) {
+    public static String getVersionName() {
         try {
-            return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+            return mainContext.getPackageManager().getPackageInfo(mainContext.getPackageName(), 0).versionName;
         } catch (PackageManager.NameNotFoundException e) {
             throw new RuntimeException("Could not get version name", e);
         }
     }
 
-    public static int getVersionCode(Context context) {
+    public static int getVersionCode() {
         try {
-            return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionCode;
+            return mainContext.getPackageManager().getPackageInfo(mainContext.getPackageName(), 0).versionCode;
         } catch (PackageManager.NameNotFoundException e) {
             throw new RuntimeException("Could not get version code", e);
         }
